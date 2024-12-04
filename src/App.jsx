c708db0a643f0d22327456e1ebbf2e7a955b583e
SMFaysalHaque <smfaysalhaque007@gmail.com>
2024-12-04 19:34:39 +0600

    style: add style for align center

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-export default function App() {
+import Index from "./components/Index.jsx";
+
+function App() {
   return (
-    <div>
-      <p>aaa</p>
-      <p>ppp</p>
+    <div className="w-full md:w-[600px] px-4 md:px-0 mt-20 mx-auto">
+      <p className="text-center mb-5">To-Do App</p>
+      <Index />
     </div>
   );
 }
+
+export default App;
---
+++ src/components/Index.jsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div className="border border-gray-600 rounded">
+      <p>aaa</p>
+      <p>ppp</p>
+    </div>
+  );
+}
